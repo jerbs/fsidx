10b5cf6f197cf5384a72ea3fa202367e570bb534
Joachim Erbs <joachim.erbs@gmx.de>
2022-02-20 23:48:52 +0100

    Colored output

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -11,6 +11,7 @@ use std::io::{Error, ErrorKind, Result, stdout, stderr, Write};
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 use crate::config::{Config, find_and_load, get_volume_info, load_from_path};
 use crate::expand::{Expand, MatchRule};
 use crate::tokenizer::{tokenize, TokenIterator, Token};
@@ -81,7 +82,8 @@ pub fn main() -> i32 {
     let exit_code = match result {
         Ok(exit_code) => exit_code,
         Err(err) => {
-            let _ = writeln!(stderr().lock(), "Error: {}", err);
+            print_error();
+            eprintln!("{}", err);
             1
         },
     };
@@ -185,53 +187,57 @@ fn locate_filter_interactive(mut token_it: TokenIterator) -> Result<Vec<FilterTo
     Ok(filter)
 }
 
-fn print_locate_result(res: &LocateResult) -> Result<()> {
+fn print_locate_result(stdout: &mut StandardStream, res: &LocateResult) -> Result<()> {
     match *res {
         LocateResult::Entry(path, Metadata { size: Some(size) } ) => {
-            stdout().write_all(path.as_os_str().as_bytes())?;
-            stdout().write_fmt(format_args!(" ({})", size))?;
-            stdout().write_all(b"\n")?;
+            stdout.write_all(path.as_os_str().as_bytes())?;
+            stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
+            stdout.write_fmt(format_args!(" ({})", size))?;
+            stdout.set_color(&ColorSpec::new())?;
+            stdout.write_all(b"\n")?;
         },
         LocateResult::Entry(path, Metadata { size:None } ) => {
-            stdout().write_all(path.as_os_str().as_bytes())?;
-            stdout().write_all(b"\n")?;
+            stdout.write_all(path.as_os_str().as_bytes())?;
+            stdout.write_all(b"\n")?;
         },
         LocateResult::Finished => {},
         LocateResult::Interrupted => {
-            stdout().write(b"CTRL-C\n")?;
+            stdout.write(b"CTRL-C\n")?;
         },
         LocateResult::Searching(path) => {
             if verbosity() {
-                stdout().write_all(b"Searching: ")?;
-                stdout().write_all(path.as_os_str().as_bytes())?;
-                stdout().write_all(b"\n")?;
+                stdout.write_all(b"Searching: ")?;
+                stdout.write_all(path.as_os_str().as_bytes())?;
+                stdout.write_all(b"\n")?;
             }
         },
         LocateResult::SearchingFinished(path) => {
             if verbosity() {
-                stdout().write_all(b"Searching  ")?;
-                stdout().write_all(path.as_os_str().as_bytes())?;
-                stdout().write_all(b" finished\n")?;
+                stdout.write_all(b"Searching  ")?;
+                stdout.write_all(path.as_os_str().as_bytes())?;
+                stdout.write_all(b" finished\n")?;
             }
         },
         LocateResult::SearchingFailed(path, error) => {
-            stdout().write_all(b"Searching ")?;
-            stdout().write_all(path.as_os_str().as_bytes())?;
-            stdout().write_fmt(format_args!(" failed: {}\n", error))?;
+            stdout.write_all(b"Searching ")?;
+            stdout.write_all(path.as_os_str().as_bytes())?;
+            stdout.write_fmt(format_args!(" failed: {}\n", error))?;
         },
     }
     Ok(())
 }
 
 fn locate(config: &Config, matches: &ArgMatches, interrupt: Option<Arc<AtomicBool>>) -> Result<i32> {
+    let mut stdout = StandardStream::stdout(ColorChoice::Auto);
     let filter_token = locate_filter(matches);
     locate_impl(config, filter_token, interrupt, |res| {
-        print_locate_result(&res)
+        print_locate_result(&mut stdout, &res)
     })?;
     Ok(0)
 }
 
 fn locate_interactive(config: &Config, token_it: TokenIterator, interrupt: Option<Arc<AtomicBool>>) -> Result<Vec<PathBuf>> {
+    let mut stdout = StandardStream::stdout(ColorChoice::Auto);
     let mut selection = Vec::new();
     let filter_token = locate_filter_interactive(token_it)?;
     locate_impl(config, filter_token, interrupt, |res| {
@@ -239,9 +245,11 @@ fn locate_interactive(config: &Config, token_it: TokenIterator, interrupt: Optio
             let pb = path.to_path_buf();
             selection.push(pb);
             let index = selection.len();
-            stdout().write_fmt(format_args!("{}. ", index))?;
+            stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
+            stdout.write_fmt(format_args!("{}. ", index))?;
+            stdout.set_color(&ColorSpec::new())?;
         }
-        print_locate_result(&res)
+        print_locate_result(&mut stdout, &res)
     })?;    
     Ok(selection)
 }
@@ -278,14 +286,15 @@ fn shell(config: Config, _matches: &ArgMatches, _sub_matches: &ArgMatches) -> Re
         let history = Path::new(&db_path).join("history.txt");
         if let Err(err) = rl.load_history(&history) {
             if matches!(err, ReadlineError::Errno(nix::errno::Errno::ENOENT)) {
-                eprintln!("Error: Reading '{}' failed: {}", history.display(), err);
+                print_error();
+                eprintln!("Reading '{}' failed: {}", history.display(), err);
             }
         }
         Some(history)
     } else {
         None
     };
-    short_help();
+    let _ = short_help();
     let mut selection: Option<Vec<PathBuf>> = None;
     loop {
         let readline = rl.readline("> ");
@@ -300,7 +309,7 @@ fn shell(config: Config, _matches: &ArgMatches, _sub_matches: &ArgMatches) -> Re
                         match err.kind() {
                             ErrorKind::Interrupted => {println!("CTRL-C");},
                             ErrorKind::BrokenPipe => {println!("EOF");},
-                            _ => {println!("Error: {}", err);},
+                            _ => {print_error(); eprintln!("{}", err);},
                         }
                     },
                 };
@@ -313,7 +322,8 @@ fn shell(config: Config, _matches: &ArgMatches, _sub_matches: &ArgMatches) -> Re
                 break
             },
             Err(err) => {
-                println!("Error: {}", err);
+                print_error();
+                eprintln!("{}", err);
                 break
             }
         }
@@ -331,8 +341,8 @@ fn process_shell_line(config: &Config, line: &str, interrupt: Arc<AtomicBool>, s
             "q" if token_it.next().is_none() => {process::exit(0);},
             "o" => {open_backslash_command(token_it, selection)?;},
             "u" if token_it.next().is_none() => {update(config)?;},
-            "h" => {help();},
-            _ => {short_help();},
+            "h" => {let _ = help();},
+            _ => {let _ = short_help();},
         };
         return Ok(None);
     }
@@ -368,20 +378,25 @@ fn open_backslash_command(token_it: TokenIterator, selection: &Option<Vec<PathBu
                                 let path = Path::new(path);
                                 open_append(&mut command, path, &mut found)?;
                             } else {
-                                eprintln!("Error: Invalid index '{}'.", index);
+                                print_error();
+                                eprintln!("Invalid index '{}'.", index);
                             }
                         } else {
-                            println!("Error: Invalid index '{}'.", index);
+                            print_error();
+                            println!("Invalid index '{}'.", index);
                         }
                     } else {
-                        eprintln!("Error: Invalid index '{}'.", text);
+                        print_error();
+                        eprintln!("Invalid index '{}'.", text);
                     }
                 },
                 crate::tokenizer::Token::Backslash(text) => {
-                    eprintln!("Error: No backslash command '\\{}' expected.", text);
+                    print_error();
+                    eprintln!("No backslash command '\\{}' expected.", text);
                 },
                 crate::tokenizer::Token::Option(text) => {
-                    eprintln!("Error: Invalid option '-{}'.", text);
+                    print_error();
+                    eprintln!("Invalid option '-{}'.", text);
                 },
             }
         }
@@ -389,7 +404,8 @@ fn open_backslash_command(token_it: TokenIterator, selection: &Option<Vec<PathBu
             open_spawn(&mut command)?;
         }
     } else {
-        eprintln!("Error: Run a query first.");
+        print_error();
+        eprintln!("Run a query first.");
     }
     Ok(())
 }
@@ -414,7 +430,8 @@ fn open_index_command(config: &Config, token_it: TokenIterator, selection: &Opti
             open_spawn(&mut command)?;
         }
     } else {
-        eprintln!("Error: Run a query first.");
+        print_error();
+        eprintln!("Run a query first.");
     }
     Ok(())
 }
@@ -428,9 +445,10 @@ fn open_append(command: &mut Command, path: &Path, found: &mut bool) -> Result<(
         stdout().write(b"'\n")?;
     }
     else {
-        stderr().write(b"Error: '")?;
-        stderr().write(path.as_os_str().as_bytes())?;
-        stderr().write(b"' not exists. Device not mounted.\n")?;
+        print_error();
+        stderr().write_all(b"'")?;
+        stderr().write_all(path.as_os_str().as_bytes())?;
+        stderr().write_all(b"' not exists. Device not mounted.\n")?;
     }
     Ok(())
 }
@@ -440,48 +458,93 @@ fn open_spawn(command: &mut Command) -> Result<()> {
     let mut child = command.spawn()?;
     let exit_status = child.wait()?;
     if !exit_status.success() {
-        eprintln!("Error: Open failed.")
+        print_error();
+        eprintln!("Open failed.");
     }
     Ok(())
 }
 
-fn short_help() {
-    println!(r#"  Ctrl-C: Interrupt printing results"#);
-    println!(r#"  Ctrl-D: Terminate application"#);
-    println!(r#"Commands:"#);
-    println!(r#"  \h               -- print detailed help"#)
+fn short_help() -> Result<()> {
+    let indent = 20;
+    let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+    write_flags(&mut stdout, &[r#"Ctrl-C"#], indent, "Interrupt printing results")?;
+    write_flags(&mut stdout, &[r#"Ctrl-D"#], indent, "Terminate application")?;
+    write_section(&mut stdout, "Commands:")?;
+    write_flags(&mut stdout, &[r#"\h"#], indent, "print detailed help")?;
+    Ok(())
 }
 
-fn help() {
-    short_help();
-    println!(r#"  \q               -- quit application"#);
-    println!(r#"  \o [id ...]      -- open files with id from last selection"#);
-    println!(r#"  \u               -- update database"#);
-    println!(r#"Searching the file system index:"#);
-    println!(r#"  text             -- Search for any path containing 'text'"#);
-    println!(r#"  foo bar          -- Path must contains all strings in any order"#);
-    println!(r#"  "foo bar"        -- Match text with spaces"#);
-    println!(r#"  "ab cb"          -- Match "ab cd", "ab-cd", "ab_cd" and "abcd""#);
-    println!(r#"  ab\ cb"          -- Match "ab cd", "ab-cd", "ab_cd" and "abcd""#);
-    println!(r#"  ab-cb            -- Match "ab cd", "ab-cd", "ab_cd" and "abcd""#);
-    println!(r#"  ab_cb            -- Match "ab cd", "ab-cd", "ab_cd" and "abcd""#);
-    println!(r#"  "\""  or  ""\"   -- Match double quote"#);
-    println!(r#"  "-"   or  ""-    -- Match dash"#);
-    println!(r#"  "\\"  or  ""\\   -- Match backslash"#);
-    println!(r#"Search options:"#);
-    println!(r#"  -case_sensitive    or  -c   -- Subsequent [text] arguments are matched case sensitive"#);
-    println!(r#"  -case_insensitive  or  -i   -- Subsequent [text] arguments are matched case insensitive"#);
-    println!(r#"  -any_order         or  -a   -- Subsequent [text] arguments may  match in any order"#);
-    println!(r#"  -same_order        or  -s   -- Subsequent [text] arguments must match in the same order"#);
-    println!(r#"  -whole_path        or  -a   -- Subsequent [text] arguments may  appear in the whole path"#);
-    println!(r#"  -last_element      or  -l   -- Subsequent [text] arguments must appear in the last element only"#);
-    println!(r#"  -                           -- Not an option. This matches a dash."#);
-    println!(r#"Open search results with index commands:"#);
-    println!(r#"  12.           -- Open single selected file"#);
-    println!(r#"  12..          -- Open all selected files in same directory"#);
-    println!(r#"  12...         -- Open all files in same directory"#);
-    println!(r#"  12..jpg       -- Open all selected files in same directory with suffix"#);
-    println!(r#"  12...jpg      -- Open all files in same directory with suffix"#);
+fn help() -> Result<()>{
+    short_help()?;
+    let indent = 20;
+    let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+    write_flags(&mut stdout, &[r#"\q"#], indent, "quit application")?;
+    write_flags(&mut stdout, &[r#"\o [id ...]"#], indent, "open files with id from last selection")?;
+    write_flags(&mut stdout, &[r#"\u"#], indent, "update database")?;
+
+    write_section(&mut stdout, "Searching the file system index:")?;
+    write_flags(&mut stdout, &[r#"text"#], indent, "Search for any path containing 'text'")?;
+    write_flags(&mut stdout, &[r#"foo bar"#], indent, "Path must contains all strings in any order")?;
+    write_flags(&mut stdout, &[r#""foo bar""#], indent, "Match text with spaces")?;
+    write_flags(&mut stdout, &[r#""ab cb""#], indent, r#"Match "ab cd", "ab-cd", "ab_cd" and "abcd""#)?;
+    write_flags(&mut stdout, &[r#"ab\ cb"#], indent, r#"Match "ab cd", "ab-cd", "ab_cd" and "abcd""#)?;
+    write_flags(&mut stdout, &[r#"ab-cb"#], indent, r#"Match "ab cd", "ab-cd", "ab_cd" and "abcd""#)?;
+    write_flags(&mut stdout, &[r#"ab_cb"#], indent, r#"Match "ab cd", "ab-cd", "ab_cd" and "abcd""#)?;
+    write_flags(&mut stdout, &[r#""\"""#, r#"""\""#], indent, "Match double quote")?;
+    write_flags(&mut stdout, &[r#""-""#, r#"""-"#], indent, "Match dash")?;
+    write_flags(&mut stdout, &[r#""\\""#, r#"""\\"#], indent, "Match backslash")?;
+    
+    let indent = 30;
+    write_section(&mut stdout, "Search options:")?;
+    write_flags(&mut stdout, &["-c", "-case_sensitive"], indent, "Subsequent [text] arguments are matched case sensitive")?;
+    write_flags(&mut stdout, &["-i", "-case_insensitive"], indent, "Subsequent [text] arguments are matched case insensitive")?;
+    write_flags(&mut stdout, &["-a", "-any_order"], indent, "Subsequent [text] arguments may  match in any order")?;
+    write_flags(&mut stdout, &["-s", "-same_order"], indent, "Subsequent [text] arguments must match in the same order")?;
+    write_flags(&mut stdout, &["-w", "-whole_path"], indent, "Subsequent [text] arguments may  appear in the whole path")?;
+    write_flags(&mut stdout, &["-l", "-last_element"], indent, "Subsequent [text] arguments must appear in the last element only")?;
+    write_flags(&mut stdout, &["-", ""], indent, "Not an option. This matches a dash.")?;
+
+    let indent = 20;
+    write_section(&mut stdout, "Open search results with index commands:")?;
+    write_flags(&mut stdout, &[r#"12."#], indent, "Open single selected file")?;
+    write_flags(&mut stdout, &[r#"12.."#], indent, "Open all selected files in same directory")?;
+    write_flags(&mut stdout, &[r#"12..."#], indent, "Open all files in same directory")?;
+    write_flags(&mut stdout, &[r#"12.."#], indent, "Open all selected files in same directory with suffix")?;
+    write_flags(&mut stdout, &[r#"12...jpg"#], indent, "Open all files in same directory with suffix")?;
+    Ok(())
+}
+
+fn write_section(stdout: &mut StandardStream, text: &str) -> Result<()> {
+    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)))?;
+    writeln!(stdout, "\n{}", text)?;
+    stdout.set_color(&ColorSpec::new())?;
+    Ok(())
+}
+
+fn write_flags(stdout: &mut StandardStream, flags: &[&str], indent: usize, description: &str) -> Result<()> {
+    let mut pos = 4;
+    write!(stdout, "    ")?;
+    for (index, flag) in flags.iter().enumerate() {
+        stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
+        write!(stdout, "{}", flag)?;
+        pos = pos + flag.chars().count();
+        stdout.set_color(&ColorSpec::new())?;
+        if index + 1 != flags.len() {
+            write!(stdout, ", ")?;
+            pos = pos + 2;
+        }
+    }
+    while pos < indent {write!(stdout, " ")?; pos = pos + 1;}
+    writeln!(stdout, "{}", description)?;
+    Ok(())
+}
+
+fn print_error() {
+    let mut stderr = StandardStream::stderr(ColorChoice::Auto);
+    let _ = stderr.set_color(ColorSpec::new().set_fg(Some(Color::Red)));
+    let _ = stderr.write_all(b"Error");
+    let _ = stderr.set_color(&ColorSpec::new());
+    let _ = stderr.write_all(b": ");
 }
 
 fn update_cli() -> clap::Command<'static> {
