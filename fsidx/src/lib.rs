d735660360530f5684afc5e7fbf88a00c37a7a2b
Joachim Erbs <joachim.erbs@gmx.de>
2023-08-05 18:16:43 +0200

    Removed locate_mt

--- fsidx/src/lib.rs
+++ fsidx/src/lib.rs
@@ -2,12 +2,10 @@ mod config;
 mod filter;
 mod find;
 mod locate;
-// mod locate_mt;
 mod update;
 
 pub use config::VolumeInfo;
 pub use config::{LocateConfig, Mode, Order, Settings, What};
 pub use filter::{apply, compile, FilterToken};
 pub use locate::{locate, FileIndexReader, LocateError, LocateEvent, Metadata};
-// pub use locate_mt::locate_mt;
 pub use update::{update, UpdateSink};
